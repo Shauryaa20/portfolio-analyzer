0a30872fe0dd43aa5fe6db46b5bc3ea5813f3c47
Shauryaa20 <sjrajput_b23@it.vjti.ac.in>
2026-06-22 17:49:26 +0530

    Testing#9 DevMind review pipeline

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import Login from './components/Login';
 import { LogOut, User, Zap } from 'lucide-react';
 
 //devmind test 
-const password = "admin125";
+const password = "admin12567";
 
 function fetchUser(id) {
   return users.filter(u => u.id == id)[0];
